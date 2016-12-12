163ebe8cad5fd1e6abb6ddebb10176d25c57fbc3
Brandon Beck <bmbeck@gmail.com>
2016-12-12 13:45:39 -0600

    Making the demo application a bit more real.
    In order to seem more real we add a couple of common bundles to the
    demo application.  The first bundle is the version bundle -- with
    this bundle the application will listen to a `/version` request on
    the admin port and return with a JSON object that describes all of
    the dependencies compiled into the jar as well as their versions.
    The second bundle is the datadog metrics bundle -- with this bundle
    the application can include configuration in its config file to
    have metrics periodically sent to datadog.

--- application/src/main/java/com/mainstreethub/demo/DemoApplication.java
+++ application/src/main/java/com/mainstreethub/demo/DemoApplication.java
@@ -2,11 +2,22 @@ package com.mainstreethub.demo;
 
 import com.mainstreethub.demo.resources.HelloEnvironmentResource;
 import io.dropwizard.Application;
+import io.dropwizard.bundles.version.VersionBundle;
+import io.dropwizard.bundles.version.suppliers.MavenVersionSupplier;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class DemoApplication extends Application<DemoConfiguration> {
+  private static final String GROUP_ID = "com.mainstreethub.demo";
+  private static final String ARTIFACT_ID = "application";
+
   public static void main(String[] args) throws Exception {
     new DemoApplication().run(args);
+  }
+
+  @Override
+  public void initialize(Bootstrap<DemoConfiguration> bootstrap) {
+    bootstrap.addBundle(new VersionBundle(new MavenVersionSupplier(GROUP_ID, ARTIFACT_ID)));
   }
 
   @Override
